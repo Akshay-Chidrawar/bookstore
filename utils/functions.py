4f194bf2a964b715351e9f183d85e2fff1e38ca5
akshayuchidrawar@gmail.com <akshayuchidrawar@gmail.com>
2026-05-31 10:40:40 +0000

    commit

--- utils/functions.py
+++ utils/functions.py
@@ -24,6 +24,7 @@ def raw_feed(spark,path_,schema_,lakeflow=1):
             .format('json')
             .option('recursiveFileLookup','false')
             .schema(schema_)
+            .mergeSchema(True)
             .load(path_)
         )
     else:
